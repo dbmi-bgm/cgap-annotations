e2c6ecb728f363705789281d878d7a810df60174
phil-grayson <>
2021-10-26 14:00:10 -0400

    updated pytest following addition of bgzip and tbi

--- variants/scripts/test/test_depth_filter.py
+++ variants/scripts/test/test_depth_filter.py
@@ -3,6 +3,7 @@
 #################################################################
 import sys, os
 import pytest
+from subprocess import Popen
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
@@ -16,20 +17,33 @@ from depth_filter import (
 
 def test_depth_filter_proband_only():
     # Variables
-    args = {'inputSampleVCF': 'test/files/depth_test_file_proband.vcf', 'outputfile': 'proband3.vcf', 'min_depth': '3'}
+    args = {'inputSampleVCF': 'test/files/depth_test_file_proband.vcf.gz', 'outputfile': 'proband3.vcf', 'min_depth': '3'}
     # Run
     main_depth_filter(args)
     # Tests
-    assert [row for row in open('proband3.vcf')] == [row for row in open('test/files/depth_test_file_proband_3.vcf')]
+
+    a = os.popen('bgzip -c -d proband3.vcf.gz')
+    b = os.popen('bgzip -c -d test/files/depth_test_file_proband_3.vcf.gz')
+
+    assert [row for row in a.read()] == [row for row in b.read()]
+
     # Clean
-    os.remove('proband3.vcf')
+    os.remove('proband3.vcf.gz')
+    os.remove('proband3.vcf.gz.tbi')
+
 
 def test_depth_filter_trio():
     # Variables
-    args = {'inputSampleVCF': 'test/files/depth_test_file_trio.vcf', 'outputfile': 'trio3.vcf', 'min_depth': '3'}
+    args = {'inputSampleVCF': 'test/files/depth_test_file_trio.vcf.gz', 'outputfile': 'trio3.vcf', 'min_depth': '3'}
     # Run
     main_depth_filter(args)
+
     # Tests
-    assert [row for row in open('trio3.vcf')] == [row for row in open('test/files/depth_test_file_trio_3.vcf')]
+    a = os.popen('bgzip -c -d trio3.vcf.gz')
+    b = os.popen('bgzip -c -d test/files/depth_test_file_trio_3.vcf.gz')
+
+    assert [row for row in a.read()] == [row for row in b.read()]
+
     # Clean
-    os.remove('trio3.vcf')
+    os.remove('trio3.vcf.gz')
+    os.remove('trio3.vcf.gz.tbi')
